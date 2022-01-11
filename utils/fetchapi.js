06e381845f91a9fde379e96884f3f9ee13630ab4
Romuel Webdev <johnromuelmendoza@yahoo.com>
2022-01-11 16:26:37 +0800

    update env file

--- utils/fetchapi.js
+++ utils/fetchapi.js
@@ -7,7 +7,7 @@ export const fetchApi = async (url) => {
     const { data }  = await axios.get((url), {
         headers: {
             'x-rapidapi-host': 'bayut.p.rapidapia.com',
-            'x-rapidapi-key': '81834b4846msh63edc329e9374fcp1c5735jsnf0ba8722fbbf'
+            'x-rapidapi-key': process.env.NEXT_PUBLIC_RAPID_API_KEY,
         }
 
     });
